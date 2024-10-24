7d09f9e2865f7239b7f91c913b149fa7d5c144e9
Noy-98 <noy.services01@gmail.com>
2024-10-25 02:36:55 +0800

    Initial commit

---
+++ app/src/main/java/com/itech/cdmrollhub/MainActivity.kt
@@ -0,0 +1,31 @@
+package com.itech.cdmrollhub
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.appcompat.app.AppCompatActivity
+import com.itech.cdmrollhub.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val fadeInAnimation = android.view.animation.AnimationUtils.loadAnimation(this, R.anim.fade_in)
+        val scaleAnimation = android.view.animation.AnimationUtils.loadAnimation(this, R.anim.scale)
+
+        binding.main.startAnimation(fadeInAnimation)
+        binding.main.startAnimation(scaleAnimation)
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            startActivity(Intent(this, LoginActivity::class.java))
+            finish()
+        }, 3000)
+
+    }
+}
